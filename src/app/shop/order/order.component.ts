95fb43e0dce15e572e2ebd34b5274d4881a57fca
Dasha <dvoretska4@gmail.com>
2018-11-12 23:31:10 +0200

    Implement jspdf to order component.

--- src/app/shop/order/order.component.ts
+++ src/app/shop/order/order.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-order',
@@ -11,10 +13,28 @@ export class OrderComponent implements OnInit {
   name: string  = '';
   email: string  = '';
   comment: string  = '';
-
+  @ViewChild('productForm') productForm:ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  downloadPdf() {
+    //   doc.save(`Order${Date.now()}.pdf`);
+
+     var data = document.getElementById('form');
+    html2canvas(data).then(canvas => {
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('MYPdf.pdf'); // Generated PDF
+    });
   }
 
 }
