0f9156c946fa0712c2eaba9aff71ff94baed290f
Dasha <dvoretska4@gmail>
2019-02-10 17:59:01 +0200

    Deploy to heroku

--- src/app/blog/comment.model.ts
+++ src/app/blog/comment.model.ts
@@ -1,3 +1,7 @@
 export class Comment {
-  constructor(public id: number, public text: string, public created: string, public user: string) {}
+  constructor(public id: number,
+  public text: string,
+  public email: string,
+  public created: string,
+  public user: string) {}
 }
