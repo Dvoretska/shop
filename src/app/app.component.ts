518bfe2aeee0039c94ade9622c9a2e3cf478b75e
Dasha <dvoretska4@gmail.com>
2019-02-20 23:24:08 +0200

    Redirect from http to https.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-project';
   constructor(private router: Router, private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    if (environment.production) {
+     if (location.protocol === 'http:') {
+      window.location.href = location.href.replace('http', 'https');
+     }
+    }
+  }
 }
